9e05e9bcfbb3b99c9e486a2d2313749e291e63a6
Trent <Trent@fuku.local>
2017-06-04 03:40:28 -0400

    waveshaper specifically for func_gen

---
+++ wrShaper.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+
+// verbose names for switch statement
+#define SQ_LOG   0
+#define LOG_TRI  1
+#define TRI_EXP  2
+#define EXP_SINE 3
+
+extern const float log_lut[];
+
+typedef struct shaper{
+	uint16_t b_size;	// size of audio vector
+	uint16_t chans;		// channel count for parallelization
+	uint8_t* zone;		// vector of interp regions
+	float* coeff;		// vector of interp coefficients
+} shaper_t;
+
+void shaper_init( shaper_t* self, uint16_t b_size, uint16_t channels );
+void shaper_prep( shaper_t* self, float control );
+void shaper_prep_v( shaper_t* self, float* audio, float control );
+float shaper_apply( shaper_t* self, float input );
+void shaper_apply_v( shaper_t* self, float* input, float* output );
+void shaper_v_p( shaper_t* self, float* inputs, float* outputs );
