428219c2b1ce5682a0c5351f473939b097a495f6
Trent <Trent@fuku.local>
2017-08-17 15:58:04 -0400

    added public defines for mode settings. modes have setter functions. fnptr used to select codeblock based on modes. explicit float constants to avoid gcc treating as doubles

--- wrLpGate.c
+++ wrLpGate.c
@@ -1,13 +1,46 @@
 #include "wrLpGate.h"
- 
+
+#include "../STM32F4-workarea/Project/JF3-test/lib/debug.h"
+
+// private declarations
+void _lpgate_mode_select( lpgate_t* self );
+// vector modes
+void lpgate_v_hpf_filt( lpgate_t* self, float* level, float* audio, float* out );
+void lpgate_v_hpf_gate( lpgate_t* self, float* level, float* audio, float* out );
+void lpgate_v_filt(     lpgate_t* self, float* level, float* audio, float* out );
+void lpgate_v_gate(     lpgate_t* self, float* level, float* audio, float* out );
+
 void lpgate_init( lpgate_t* self, uint8_t hpf, uint8_t filter, uint16_t b_size )
 {
 	self->hpf = hpf;
 	self->filter = filter;
+	_lpgate_mode_select(self);
+
 	self->b_size = b_size;
 
 	self->prev_lo = 0;
 	self->prev_hi = 0;
+}
+
+void lpgate_hpf_mode( lpgate_t* self, uint8_t hpf )
+{
+	self->hpf = !!hpf; // force 0/1
+	_lpgate_mode_select(self);
+}
+void lpgate_filter_mode( lpgate_t* self, uint8_t filter )
+{
+	self->filter = !!filter; // force 0/1
+	_lpgate_mode_select(self);
+}
+void _lpgate_mode_select( lpgate_t* self )
+{
+	static void (*fnptr[4])() =
+		{ lpgate_v_gate
+		, lpgate_v_filt
+		, lpgate_v_hpf_gate
+		, lpgate_v_hpf_filt
+		};
+	self->lpgate_fnptr = fnptr[ self->filter | (self->hpf << 1) ];
 }
 
 float lpgate_step( lpgate_t* self, float level, float in )
@@ -18,10 +51,10 @@ float lpgate_step( lpgate_t* self, float level, float in )
 		out_lo = self->prev_lo +
 					(level * 
 					(in - self->prev_lo));
-		out_lo *= level/(0.1 + level) + LOG_VOL_CONST;
+		out_lo *= level/(0.1f + level) + LOG_VOL_CONST;
 	} else {
 		out_lo = self->prev_lo +
-					((0.5 + level*0.5) * 
+					((0.5f + level*0.5f) * 
 					(in - self->prev_lo));
 		out_lo *= level;
 	}
@@ -36,7 +69,17 @@ float lpgate_step( lpgate_t* self, float level, float in )
 	return out_hi;
 }
 
+
 void lpgate_v( lpgate_t* self, float* level, float* audio, float* out )
+{
+	// call the fnptr inside
+	(*self->lpgate_fnptr)( self
+		                 , level
+		                 , audio
+		                 , out );
+}
+
+void lpgate_v_hpf_filt( lpgate_t* self, float* level, float* audio, float* out )
 {
 	float lpf[self->b_size]; // allows sequential processing
 	float* lpf2 = lpf;
@@ -47,50 +90,138 @@ void lpgate_v( lpgate_t* self, float* level, float* audio, float* out )
 	float* out2 = out;
 	uint16_t i;
 
-	// first sample
-	if(self->filter){ // BOTH MODE (LPF -> VOL)
-		*lpf2 = self->prev_lo +
+	*lpf2 = self->prev_lo +
+				(*level2 * 
+				(*in2++ - self->prev_lo));
+	*lpf2++ *= *level2 / (0.1f + *level2) + LOG_VOL_CONST;
+// divide is too expensive
+// below is an alternative which goes half-way to matching curves
+// but it's still hugely expensive
+// let's not worry about changing the curve for now...
+	// *lpf2++ *= 1.8 * (*level2 - *level2 * *level2 * 0.5) + LOG_VOL_CONST;
+	level2++;
+
+	for(i=1; i<(self->b_size); i++){
+		*lpf2 = *lpf3 +
 					(*level2 * 
-					(*in2++ - self->prev_lo));
-		*lpf2++ *= *level2/(0.1 + *level2) + LOG_VOL_CONST;
+					(*in2++ - *lpf3));
+		*lpf2++ *= *level2 / (0.1f + *level2) + LOG_VOL_CONST;
+		// *lpf2++ *= 1.8 * (*level2 - *level2 * *level2 * 0.5) + LOG_VOL_CONST;
+		lpf3++;
 		level2++;
-		for(i=1; i<(self->b_size); i++){
-			*lpf2 = *lpf3 +
-						(*level2 * 
-						(*in2++ - *lpf3));
-			*lpf2++ *= *level2/(0.1 + *level2) + LOG_VOL_CONST;
-			lpf3++;
-			level2++;
-		}
-	} else { // VCA MODE (subtle LPF)
-		*lpf2 = self->prev_lo +
-					((0.5 + *level2*0.5) * 
-					(*in2++ - self->prev_lo));
-		*lpf2++ *= *level2++;
-		for(i=1; i<(self->b_size); i++){
-			*lpf2 = *lpf3 +
-						((0.5 + *level2*0.5) * 
-						(*in2++ - *lpf3));
-			*lpf2++ *= *level2++;
-			lpf3++;
-		}
 	}
 
+	// hpf
 	lpf2 = lpf;
-	if(self->hpf){ // HPF ACTIVE
-		lpf3 = lpf;
-		float* out3 = out;
-		*out2++ = *lpf2++ - self->prev_lo + (HPF_COEFF * self->prev_hi);
-		for(i=1; i<(self->b_size); i++){
-			*out2++ = *lpf2++ - *lpf3++ + (HPF_COEFF * *out3++);
-		}
-		self->prev_lo = *lpf3;
-		self->prev_hi = *out3;
-	} else{ // PASS THROUGH
-		for(i=0; i<(self->b_size); i++){
-			*out2++ = *lpf2++;
-		}
-		self->prev_lo = *(--lpf2);
-		self->prev_hi = *(--out2);
+	lpf3 = lpf;
+	float* out3 = out;
+	
+	*out2++ = *lpf2++ - self->prev_lo + (HPF_COEFF * self->prev_hi);
+	
+	for(i=1; i<(self->b_size); i++){
+		*out2++ = *lpf2++ - *lpf3++ + (HPF_COEFF * *out3++);
 	}
+	
+	self->prev_lo = *lpf3;
+	self->prev_hi = *out3;
+}
+void lpgate_v_hpf_gate( lpgate_t* self, float* level, float* audio, float* out )
+{
+	float lpf[self->b_size]; // allows sequential processing
+	float* lpf2 = lpf;
+	float* lpf3 = lpf;
+
+	float* level2 = level;
+	float* in2 = audio;
+	float* out2 = out;
+	uint16_t i;
+
+	*lpf2 = self->prev_lo +
+				((0.5f + *level2*0.5f) * 
+				(*in2++ - self->prev_lo));
+	*lpf2++ *= *level2++;
+
+	for(i=1; i<(self->b_size); i++){
+		*lpf2 = *lpf3 +
+					((0.5f + *level2*0.5f) * 
+					(*in2++ - *lpf3));
+		*lpf2++ *= *level2++;
+		lpf3++;
+	}
+	
+	// hpf
+	lpf2 = lpf;
+	lpf3 = lpf;
+	float* out3 = out;
+
+	*out2++ = *lpf2++ - self->prev_lo + (HPF_COEFF * self->prev_hi);
+	
+	for(i=1; i<(self->b_size); i++){
+		*out2++ = *lpf2++ - *lpf3++ + (HPF_COEFF * *out3++);
+	}
+	
+	self->prev_lo = *lpf3;
+	self->prev_hi = *out3;
+}
+void lpgate_v_filt(     lpgate_t* self, float* level, float* audio, float* out )
+{
+	float lpf[self->b_size]; // allows sequential processing
+	float* lpf2 = lpf;
+	float* lpf3 = lpf;
+
+	float* level2 = level;
+	float* in2 = audio;
+	float* out2 = out;
+	uint16_t i;
+
+	*lpf2 = self->prev_lo +
+				(*level2 * 
+				(*in2++ - self->prev_lo));
+	*lpf2 *= *level2 / (0.1f + *level2) + LOG_VOL_CONST;
+	*out2++ = *lpf2++;
+	level2++;
+
+	for(i=1; i<(self->b_size); i++){
+		*lpf2 = *lpf3 +
+					(*level2 * 
+					(*in2++ - *lpf3));
+		*lpf2 *= *level2 / (0.1f + *level2) + LOG_VOL_CONST;
+		*out2++ = *lpf2++;
+		lpf3++;
+		level2++;
+	}
+
+	self->prev_lo = *(--lpf2);
+	self->prev_hi = *(--out2);
+}
+void lpgate_v_gate( lpgate_t* self, float* level, float* audio, float* out )
+{
+	float lpf[self->b_size]; // allows sequential processing
+	float* lpf2 = lpf;
+	float* lpf3 = lpf;
+
+	float* level2 = level;
+	float* in2 = audio;
+	float* out2 = out;
+	uint16_t i;
+
+	*lpf2 = self->prev_lo +
+				((0.5f + *level2 * 0.5f) * 
+				// (*level2 * 
+				(*in2++ - self->prev_lo));
+	*lpf2 *= *level2++;
+	*out2++ = *lpf2++;
+
+	for(i=1; i<(self->b_size); i++){
+		*lpf2 = *lpf3 +
+					((0.5f + *level2 * 0.5f) * 
+					// (*level2 * 
+					(*in2++ - *lpf3));
+		*lpf2 *= *level2++;
+		*out2++ = *lpf2++;
+		lpf3++;
+	}
+
+	self->prev_lo = *(--lpf2);
+	self->prev_hi = *(--out2);
 }
--- wrLpGate.h
+++ wrLpGate.h
@@ -2,12 +2,21 @@
 
 #include <stdint.h>
 
-#define LOG_VOL_CONST 	(0.1/1.1)
-#define HPF_COEFF		(0.997)
+#define LOG_VOL_CONST 	((float)(0.1/1.1))
+#define HPF_COEFF		((float)(0.997))
+
+#define LPGATE_HPF_OFF  0
+#define LPGATE_HPF_ON	1
+#define LPGATE_VCA  	0
+#define LPGATE_FILTER	1
 
 typedef struct lpgate{
 	uint8_t hpf;
 	uint8_t filter;
+	void (*lpgate_fnptr)( struct lpgate* self
+		                , float* level
+		                , float* audio
+		                , float* out );
 	uint16_t b_size;
 
 	float prev_lo;
@@ -15,6 +24,8 @@ typedef struct lpgate{
 } lpgate_t;
 
 void lpgate_init( lpgate_t* self, uint8_t hpf, uint8_t filter, uint16_t b_size );
+void lpgate_hpf_mode( lpgate_t* self, uint8_t hpf );
+void lpgate_filter_mode( lpgate_t* self, uint8_t filter );
 	// level input expects (0-1)
 float lpgate_step( lpgate_t* self, float level, float in );
 void lpgate_v( lpgate_t* self, float* level, float* audio, float* out );
