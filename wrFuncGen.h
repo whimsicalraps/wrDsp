bcbb8b77ad3bbe10ad0177b2a2e99eda4f804539
Trent <trent.gill@gmail.com>
2017-08-23 14:53:38 -0400

    add sustaining flag and formatting

--- wrFuncGen.h
+++ wrFuncGen.h
@@ -17,18 +17,17 @@ typedef enum
     } fg_mode_t;
 
 typedef struct func_gen{
-	int8_t    go;
-	float     id;
-	float     rate;
-	uint8_t   tr_state;
-	float     fm_ix;
-	int8_t    loop;	// nb: -1 is infinite loop. +ve is number of retrigs left.
-	uint8_t   s_mode;
-	uint8_t   sustain_state;
-
-
-	float     r_up;
-	float     r_down;
+	int8_t  go;
+	float   id;
+	float   rate;
+	uint8_t tr_state;
+	float   fm_ix;
+	int8_t  loop;	// nb: -1 is infinite loop. +ve is number of retrigs left.
+	uint8_t s_mode;
+	uint8_t sustain_state;
+	uint8_t sustaining;
+	float   r_up;
+	float   r_down;
 } func_gen_t;
 
 // Initialization
@@ -38,17 +37,22 @@ void function_init( func_gen_t* self, int8_t loop );
 
 // Triggers
 void function_trig_reset  ( func_gen_t* self
-	                      , uint8_t     state );
+	                      , uint8_t     state
+	                      );
 void function_trig_wait   ( func_gen_t* self
-	                      , uint8_t     state );
+	                      , uint8_t     state
+	                      );
 void function_trig_sustain( func_gen_t* self
-	                      , uint8_t     state );
+	                      , uint8_t     state
+	                      );
 void function_trig_vari   ( func_gen_t* self
 	                      , uint8_t     state
-	                      , float       cutoff );
+	                      , float       cutoff
+	                      );
 void function_trig_burst  ( func_gen_t* self
 	                      , uint8_t     state
-	                      , float       count );
+	                      , float       count
+	                      );
 
 void function_mode( func_gen_t* self, uint8_t mode );
 void function_loop( func_gen_t* self, int8_t loop );
@@ -61,8 +65,12 @@ void function_ramp( func_gen_t* self, float skew );
 void function_ramp_v( uint16_t b_size, float ctrl_rate, float* audio_rate, float* ramp_up, float* ramp_down );
 
 float function_step( func_gen_t* self, float fm_in );
-// void function_v( trig_state[j], process[0], process[j] );
-void function_v( func_gen_t* self, uint16_t b_size, float* r_up, float* r_dn, float* fm_in, float* out );
-
+void function_v( func_gen_t* self
+	           , uint16_t b_size
+	           , float* r_up
+	           , float* r_dn
+	           , float* fm_in
+	           , float* out
+	           );
 
 float function_lookup( float id );
