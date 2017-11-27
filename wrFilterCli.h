2a882abd5e691c458fd4e917267c2ed6593e75a3
Trent <trent.gill@gmail.com>
2017-11-26 23:28:19 -0500

    wrapper fns & init

--- wrFilterCli.h
+++ wrFilterCli.h
@@ -4,3 +4,6 @@
 #include "wrFilter.h" // this wraps the main DSP lib
 
 module_t* graph_lp1_init( void );
+void g_lp1_process( module_t* box );
+void g_lp1_set_coeff( void* f, float c );
+float g_lp1_get_coeff(void* f);
